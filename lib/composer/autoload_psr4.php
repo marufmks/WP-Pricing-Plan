bedeefb8e7c164571d8cd518f321429bc088e393
marufmks <marufmks5556@gmail.com>
2025-01-24 23:21:35 +0600

    added wp cs

--- lib/composer/autoload_psr4.php
+++ lib/composer/autoload_psr4.php
@@ -7,4 +7,5 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'Pricing_Plan\\' => array($baseDir . '/core'),
+    'PHPCSStandards\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
 );
